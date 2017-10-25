1a0a887a06b0544864693f1b1b4a6ce09cfbca89
Ewan Mellor <commits@ewanmellor.org>
2017-10-25 16:09:08 -0700

    Remove debug logging and unused code.

--- iridium-to-twilio/index.js
+++ iridium-to-twilio/index.js
@@ -9,26 +9,21 @@ const TWILIO_PHONE = process.env.TWILIO_PHONE_NUMBER;
 
 
 exports.handler = (event, context, callback) => {
-    console.log("Received message!", event, context);
-
     let p = querystring.decode(event.body);
-    let imei = p.imei;
     let data = p.data;
-    
     let parsedData = hex2a(data);
     let [num, content] = splitWithTail(parsedData, ':', 1);
-    
+
     let post_params = {
         "To": num,
         "From": TWILIO_PHONE, 
         "Body": content
     };
     let post_data = querystring.encode(post_params);
-    
+
     const auth = `${TWILIO_SID}:${TWILIO_TOKEN}`;
     const auth_b64 = a2b64(auth);
-    console.log("From", post_params.From, post_data);
-    
+
     // TWILIO_URL = `https://api.twilio.com/2010-04-01/Accounts/${TWILIO_SID}/Messages.json`
     let req = https.request({
         host: "api.twilio.com",
